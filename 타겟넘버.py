79e9c1e01620da266e2a797f2f931f69d1c42c07
jhe226 <79580469+jhe226@users.noreply.github.com>
2021-08-01 21:35:35 +0900

    Update 타겟넘버.py

--- 타겟넘버.py
+++ 타겟넘버.py
@@ -15,3 +15,17 @@ def solution(numbers, target):
             if tmp == target:
                 answer += 1
     return answer
+
+
+'''
+<다른 풀이>
+# 재귀 이용
+
+    def solution(numbers, target):
+    if not numbers and target == 0 :
+        return 1
+    elif not numbers:
+        return 0
+    else:
+        return solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
+'''
